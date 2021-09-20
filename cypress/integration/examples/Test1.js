78a27c4a4504557f1adb76aee62988ed88359786
Inga Bilyk <inga11bil@gmail.com>
2021-09-19 19:55:10 -0700

    Test1Done

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -8,11 +8,15 @@ it('Test case 1', ()=>{
     cy.wait(2000)
     cy.get('.product:visible').should('have.length', 7)
 
-    cy.get('.products').find('.product').should('have.length', 7)
-    cy.get('.products').find('.product').eq(3).contains('ADD TO CART').click()
+    cy.get('.products').as('productsLocator')
+    cy.get('@productsLocator').find('.product').should('have.length', 7)
+    cy.get('@productsLocator').find('.product').eq(3).contains('ADD TO CART').click().then(function() 
+    {
+        console.log('hello')
+    })
 
     
-    cy.get('.products').find('.product').each(($el, index, $list) =>{
+    cy.get('@productsLocator').find('.product').each(($el, index, $list) =>{
 
         const vegName = $el.find('h4.product-name').text()
         if (vegName.includes('Corn'))
@@ -22,6 +26,8 @@ it('Test case 1', ()=>{
 
 
     })
+
+    cy.get('.brand').should('have.text','GREENKART')
 
     cy.get('.brand').then(function(getlogo){
 
